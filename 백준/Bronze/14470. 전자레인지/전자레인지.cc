66656dde1d93e2c57e3d8affe56a29b4dbe3834c
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2024-03-20 09:48:34 +0900

    [Bronze IV] Title: 전자레인지, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/14470. 전자레인지/전자레인지.cc
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, d, e;
+    cin>>a>>b>>c>>d>>e;
+    if (a < 0) cout<<abs(a)*c + d + b*e;
+    else cout<<(b-a)*e;
+    return 0;
+}
