474b5a2130d7454c44aae9f4094b52e59831cc02
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2024-03-21 11:49:34 +0900

    [Bronze IV] Title: HOMWRK, Time: 16 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18398. HOMWRK/HOMWRK.cc
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int t, n, a, b;
+    cin>>t;
+    while(t--)
+    {
+        cin>>n;
+        while(n--)
+        {
+            cin>>a>>b;
+            cout<<a+b << " " << a*b <<"\n";
+        }
+    }
+    return 0;
+}
