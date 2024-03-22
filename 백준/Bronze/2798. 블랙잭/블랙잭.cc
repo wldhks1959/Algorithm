931d1fb6c998bfafbac10249031788ca1606ab5d
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2024-03-22 15:09:48 +0900

    [Bronze II] Title: 블랙잭, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/2798. 블랙잭/블랙잭.cc
+++ 백준/Bronze/2798. 블랙잭/블랙잭.cc
@@ -1,25 +1,24 @@
 #include <iostream>
 using namespace std;
 
-int main() {
+int main()
+{
+	int n, m; cin >> n >> m;
 	int arr[100];
-	int N, M; cin >> N >> M;
+	int sum = 0, result=0;
+	for (int i = 0; i < n; i++) cin >> arr[i];
 
-	for (int i = 0; i < N; i++) {
-		cin >> arr[i];
-	}
-
-	int result = 0;
-	int sum = 0;
-
-	for (int i = 0; i < N - 2; i++) {
-		for (int j = i + 1; j < N - 1; j++) {
-			for (int k = j + 1; k < N; k++) {
+	for (int i = 0; i < n - 2; i++)
+	{
+		for (int j = i+1; j < n - 1; j++)
+		{
+			for (int k = j+1; k < n; k++)
+			{
 				sum = arr[i] + arr[j] + arr[k];
-				if (sum > result && sum <= M) result = sum;
+				if (result < sum && sum <= m) result = sum;
 			}
 		}
 	}
 	cout << result;
-
+	return 0;
 }
