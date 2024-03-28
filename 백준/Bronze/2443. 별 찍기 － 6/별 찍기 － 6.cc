afff11a84b30939713d527b3de4834c8445e96c0
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2024-03-28 11:30:12 +0900

    [Bronze III] Title: 별 찍기 - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2443. 별 찍기 － 6/별 찍기 － 6.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+
+int main(void)
+{
+	int n; cin >> n;
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < i; j++) cout << " ";
+		for (int j = 2 * (n - i)-1; j > 0; j--) cout << "*";
+		cout << "\n";
+	}
+	return 0;
+}
