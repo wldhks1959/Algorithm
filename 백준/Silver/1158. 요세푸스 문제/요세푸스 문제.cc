5f2e4545223b5620ba9223811649e6b393c7c4f1
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2024-04-03 14:09:08 +0900

    [Silver IV] Title: 요세푸스 문제, Time: 68 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
+++ 백준/Silver/1158. 요세푸스 문제/요세푸스 문제.cc
@@ -1,67 +1,29 @@
-#pragma warning(disable:4996)
-#include <stdio.h>
-#include <stdlib.h>
+// 1158. 요세푸스 문제
+#include <iostream>
+#include <queue>
+using namespace std;
 
-typedef struct Node
-{
-	int data;
-	struct Node* next;
-}Node;
-
-class LinkedList 
-{
-	private : 
-		Node* head = nullptr;
-	public :
-		void Push(int value) 
-		{
-			Node* newNode = (Node*)malloc(sizeof(Node));
-			newNode->data = value;
-			newNode->next = head;
-
-			// if headNode = null
-			if (head == nullptr)
-			{
-				head = newNode;
-				head->next = head;
-			}
-			else
-			{
-				Node* curr = head;
-				while (curr->next != head) curr = curr->next;
-				curr->next = newNode;
-				newNode->next = head;
-			}
-			return;
-		}
-		int Pop(int K)
-		{
-			Node* prev = head;
-			while (K--)
-			{
-				prev = head;
-				head = head->next;
-			}
-
-			int value = head->data;
-			prev->next = head->next;
-
-			head = prev->next;
-			return value;
-		}
-};
+queue<int> q;
 
 int main(void)
 {
 	int n, k;
-	LinkedList list;
-
-	scanf("%d %d", &n, &k);
-
-	for (int i = 1; i <= n; i++) list.Push(i);
-	printf("<");
-
-	for (int i = 1; i < n; i++) printf("%d, ", list.Pop(k - 1));
-	printf("%d>", list.Pop(n));
+	cin >> n >> k;
+	for (int i = 1; i <= n; i++) q.push(i);
+	
+	cout << "<";
+	while (!q.empty())
+	{
+		for (int i = 0; i < k-1; i++)
+		{
+			int tmp = q.front();
+			q.pop();
+			q.push(tmp);
+		}
+		cout << q.front();
+		q.pop();
+		if (!q.empty()) cout << ", ";
+	}
+	cout << ">" << "\n";
 	return 0;
 }
