d1a1225284ac36a49a4303cb4125a55d010c8855
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2024-04-09 09:59:47 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -0,0 +1,21 @@
+// 11727 타일링
+#include<iostream>
+using namespace std;
+#define MAX 1001
+
+int dp[MAX];
+
+int tile(int x)
+{
+	if (x == 1) return dp[x] = 1;
+	if (x == 2) return dp[x] = 3;
+	if (dp[x] != 0) return dp[x];
+	return dp[x] = (tile(x - 1) + 2*tile(x - 2)) % 10007;
+}
+
+int main(void)
+{
+	int n; cin >> n;
+	cout << tile(n);
+	return 0;
+}
