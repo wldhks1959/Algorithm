0f072173a456922fbb1033ffc6086b718d9198ec
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2024-05-15 12:16:54 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.cc
@@ -0,0 +1,14 @@
+// 20976 2번째로 큰 정수 
+
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(void)
+{
+   int arr[3];
+   for (int i = 0; i < 3; i++) cin >> arr[i];
+   sort(arr, arr + 3);
+   cout << arr[1];
+   return 0;
+}
