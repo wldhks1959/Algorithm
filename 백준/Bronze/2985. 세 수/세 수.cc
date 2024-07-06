5f84932ab995abf38427bdd689022dd71928b46e
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2024-07-06 11:23:24 +0900

    [Bronze III] Title: 세 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2985. 세 수/세 수.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+    double a, b, c;
+    cin >> a >> b >> c;
+    if (a + b == c) cout << a << '+' << b << '=' << c;
+    else if (a - b == c) cout << a << '-' << b << '=' << c;
+    else if (a * b == c) cout << a << '*' << b << '=' << c;
+    else if (a / b == c) cout << a << '/' << b << '=' << c;
+    else if (a == b + c) cout << a << '=' << b << '+' << c;
+    else if (a == b - c) cout << a << '=' << b << '-' << c;
+    else if (a == b * c) cout << a << '=' << b << '*' << c;
+    else if (a == b / c) cout << a << '=' << b << '/' << c;
+    return 0;
+}
