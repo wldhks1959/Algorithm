b91f923b80c8336a73e0108442bfd36abfe86cc8
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2024-09-12 22:16:29 +0900

    [level 2] Title: 기능개발, Time: 0.02 ms, Memory: 3.59 MB -BaekjoonHub

---
+++ 프로그래머스/2/42586. 기능개발/기능개발.cpp
@@ -0,0 +1,33 @@
+#include <string>
+#include <vector>
+#include <queue>
+using namespace std;
+
+vector<int> solution(vector<int> progresses, vector<int> speeds) 
+{
+    vector<int> answer;
+    queue<int> q;
+    
+    for(int i=0;i<progresses.size();i++)
+    {
+        if((100-progresses[i])%speeds[i]==0)
+            q.push((100-progresses[i])/speeds[i]);
+        else
+            q.push((100-progresses[i])/speeds[i]+1);
+    }
+    
+    while(!q.empty())
+    {
+        int cnt=1;
+        int cur=q.front();
+        q.pop();
+        while(!q.empty() && q.front() <= cur)
+        {
+            cnt++;
+            q.pop();
+        }
+        answer.push_back(cnt);
+    }
+    
+    return answer;
+}
