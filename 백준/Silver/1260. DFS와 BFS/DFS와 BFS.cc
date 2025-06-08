02cbd21b725bec1bde05965f13ff4e3006f7d1be
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2025-06-08 11:19:23 +0900

    [Silver II] Title: DFS와 BFS, Time: 0 ms, Memory: 2180 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,74 +1,75 @@
 #include <iostream>
 #include <queue>
-#define MAX 1001
+#include <algorithm>
+#define MAX 1002
 using namespace std;
 
-int N, M, V;
-int map[MAX][MAX]; // 인접 행렬 그래프
-bool visited[MAX];
+// 그래프 초기화
+int n, m, start_node;
+vector<int> adjList[MAX];
+bool visited[MAX] = { false };
 queue<int> q;
 
-void reset() 
-{
-	for (int i = 1; i <= N; i++) 
-		visited[i] = 0;
-}
-
-void dfs(int v)
-{
-	visited[v] = true; 
-	cout << v << " ";
-
-	for (int i = 1; i <= N; i++)
-	{
-		if (map[v][i] == 1 && visited[i] == 0) dfs(i); // 현재 정점과 연결되어있고, 방문되지 않았으면 
+void reset() {
+	for (int i = 1; i <= n; i++) {
+		visited[i] = false;
 	}
 }
 
-void bfs(int v)
-{
-	q.push(v); //대기열에 넣기 
-	visited[v] = true; //방문 처리 
-	cout << v << " ";
+void bfs(int start) {
+	q.push(start);
+	visited[start] = true;
+	printf("%d ", start);
 
-	while(!q.empty()) //q가 비어있지 않을 동안 
-	{
-		v = q.front();
+	while (!q.empty()) {
+		int cur_node = q.front();
 		q.pop();
-
-		for (int j = 1; j <= N; j++)
-		{
-			if (map[v][j] == 1 && visited[j] == 0)
-			{
-				q.push(j);
-				visited[j] = true;
-				cout<<j<<" ";
+		
+		for (int neigbor : adjList[cur_node]) {
+			if (!visited[neigbor]) {
+				q.push(neigbor);
+				visited[neigbor] = true;
+				printf("%d ", neigbor);
 			}
 		}
 	}
 }
 
-int main()
-{
-	ios_base::sync_with_stdio(false); cin.tie(0);
 
-	cin >> N >> M >> V;
 
-	for (int i = 0; i < M; i++)
-	{
-		int a, b;
-		cin >> a >> b;
-		map[a][b] = 1;
-		map[b][a] = 1;
+void dfs(int start) {
+	visited[start] = true;
+	printf("%d ", start);
+
+	for (int neighbor : adjList[start]) {
+		if (!visited[neighbor]) {
+			dfs(neighbor);
+		}
+	}
+}
+
+int main(void) {
+	
+	scanf("%d %d %d", &n, &m, &start_node);
+	// 간선 추가
+	for (int i = 0; i < m; i++){
+		int u, v;
+		scanf("%d %d", &u, &v);
+		adjList[u].push_back(v);
+		adjList[v].push_back(u);
 	}
 
-	reset();
-	dfs(V);
+	// 각 노드의 인접 리스트를 정렬
+	for (int i = 1; i <= n; i++) {
+		sort(adjList[i].begin(), adjList[i].end());
+	}
 
-	cout << "\n";
 
+	dfs(start_node);
+	printf("\n");
 	reset();
-	bfs(V);
+	bfs(start_node);
+
 
 	return 0;
 }
