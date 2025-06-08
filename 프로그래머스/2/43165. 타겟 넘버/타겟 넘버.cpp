4d36b21b9ba7a36e41d66966afb1980e36036312
Seo jiwan <69555890+wldhks1959@users.noreply.github.com>
2025-06-08 12:08:40 +0900

    [level 2] Title: 타겟 넘버, Time: 74.81 ms, Memory: 4.2 MB -BaekjoonHub

---
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.cpp
@@ -0,0 +1,20 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int ans=0;
+
+void dfs(vector<int> numbers, int target, int sum, int idx){
+    if(idx == numbers.size()) {
+        if(sum == target) ans++;
+        return;
+    }
+    dfs(numbers, target, sum+numbers[idx], idx+1);
+    dfs(numbers, target, sum-numbers[idx], idx+1);
+}
+
+int solution(vector<int> numbers, int target) {
+    dfs(numbers, target, 0, 0);
+    return ans;
+}
